394c16dcf819d089521656939b2a2fb50e91faeb
bhj2bb <cs2uee@gmail.com>
2023-10-31 09:20:21 +0900

    commit 2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,6 +4,6 @@ package com.ll;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("commit 1");
+        System.out.printf("commit 2");
     }
 }
